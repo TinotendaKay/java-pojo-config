f5ad1a63af09fd9440f92ad57e0c9b6ce9953856
Tino <Tinotenda.Kanyandura@standardbank.co.za>
2018-03-08 15:22:01 +0200

    Initial commit.

---
+++ src/main/java/com/appres/sequence/SequenceGenerator.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appres.sequence;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ *
+ * @author tinotenda
+ */
+public class SequenceGenerator {
+    private String prefix;
+    private String suffix;
+    private int initial;
+    private final AtomicInteger counter = new AtomicInteger();
+
+    public SequenceGenerator() {
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public int getInitial() {
+        return initial;
+    }
+
+    public void setInitial(int initial) {
+        this.initial = initial;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("SequenceGenerator{");
+        builder.append("prefix=").append(prefix);
+        builder.append(", suffix=").append(suffix);
+        builder.append(", initial=").append(initial);
+        builder.append(", counter=").append(counter.getAndIncrement());
+        return builder.toString();
+    }
+    
+    
+            
+    
+}
