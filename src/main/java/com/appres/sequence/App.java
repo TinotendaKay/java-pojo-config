3fb14cd9d74c95069ae98c1f65b921226d94b5f4
tinotenda.kanyandura <tinotendakanyandura@gmail.com>
2018-03-17 15:11:16 +0200

    Creating application context from the package path.

--- src/main/java/com/appres/sequence/App.java
+++ src/main/java/com/appres/sequence/App.java
@@ -10,11 +10,16 @@ public class App {
     private static final Logger LOGGER= LoggerFactory.getLogger(App.class);
 
   public static void main(String[] args) {
-      ApplicationContext context = new AnnotationConfigApplicationContext(SequenceGeneratorConfiguration.class);
+      ApplicationContext context = new AnnotationConfigApplicationContext("com.appres.sequence");
       
       SequenceGenerator sequenceGenerator = context.getBean(SequenceGenerator.class);
-      LOGGER.info("seq1: {}", sequenceGenerator.getSequence());
-      LOGGER.info("seq2: {}", sequenceGenerator.getSequence());
+      LOGGER.info("seqGen1: {}", sequenceGenerator.getSequence());
+      LOGGER.info("seq Gen2: {}", sequenceGenerator.getSequence());
+      
+        SequenceDao bean = context.getBean(SequenceDao.class);
+        
+        LOGGER.info("1 Sequence Dao next val : {}", bean.getNextValue("IT"));
+        LOGGER.info("2 Sequence Dao next val : {}", bean.getNextValue("IT"));
       
   }
 
